a3e357b23b5d8c2273e76fa9ab0fc483896202b5
Waterstrong <sqlin@thoughtworks.com>
2017-03-13 23:21:59 +0800

    finish input text practice

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-export default props => (
-  <div>
-    Hello React!
-  </div>
-)
+import Input from './input'
+
+export default props => {
+    return (
+        <div>
+            Input Text Demo:
+            <Input onChange={event => console.log(event)}
+                   onFocus={event => console.log(event)}
+                   onClick={event => console.log(event)}/>
+        </div>
+    )
+}
